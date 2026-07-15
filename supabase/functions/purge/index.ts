964cbe3a9e6699af8ec65c94f0a9a093aca31d4a
당신의이름 <you@example.com>
2026-07-15 10:28:04 +0900

    db 제거

--- supabase/functions/purge/index.ts
+++ supabase/functions/purge/index.ts
@@ -6,7 +6,6 @@ import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
 const db = createClient(
   Deno.env.get("SUPABASE_URL")!,
   Deno.env.get("SUPABASE_SERVICE_ROLE_KEY")!,
-  { db: { schema: "slda" } },
 );
 
 const storage = createClient(
